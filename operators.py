404a3f99a89c4bfa5f3d31391c6268f0b53a0dd5
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-05-31 13:54:19 +0530

    intial commit

---
+++ operators.py
@@ -0,0 +1,54 @@
+friends = 4
+#friends +=1
+#friends **=2
+remainder = friends % 4
+
+#print(remainder)
+x= 3.14 
+y=-2
+#result = round(y)
+#result = abs(y)
+#result = pow(4,3)
+#result = max(x,y)
+#print(result)
+
+
+import math
+#print(math.pi)
+#print(math.e)
+#results = math.sqrt(4)
+#results =math.ceil(x)
+#results = math.floor(x)
+#print(results)
+
+#exercise on circumference of a circle
+#radius = float(input("Enter the radius of the circle: "))
+#circumference = 2* math.pi *radius
+#print(f"the circumference is: {round(circumference, 2)}")
+
+#exercise on area of a circle
+#radius = float(input("Enter the radius of the circle: "))
+#area = math.pi * math.pow(radius,2)
+#print(f"the area is: {round(area, 2)}")
+
+
+#if else staements
+
+#age = int(input("enter your age:"))
+#if age>=19:
+#    print("you are now signrd up!:")
+#elif age<10:
+#    print("your are too young to sign up")
+#elif age>=16 and age<19:
+#    print("you should wait for a few years to signup")#
+#else:
+#    print("you must be to cross 19+ years old to sign up")
+
+
+#response = input("would you like food?  (y/n):")
+#if response == "y":
+#    print("here is your food")
+#else:
+#    print("okay, we will have next time ")
+ 
+    
