eefea712cb80d2f50b1d83515cb74dc3019fe924
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-05-31 15:23:27 +0530

    completed excercise on conversions

--- operators.py
+++ operators.py
@@ -55,23 +55,54 @@ import math
 
 #python calculator 
 
-operator = input("enter a operator (+,-,*,/)")
-num1 = float(input("enter first number: "))
-num2 = float(input("enter second number:"))
-print(f"you entered: {num1} {operator} {num2}")
-if operator == "+":
-    result = num1 + num2
-    print(f"the addtion value is : {result}")
-elif operator == "-":
-    result = num1 - num2
-    print(f"the subtraction value is : {result}")
-elif operator == "*":
-    result = num1 *num2
-    print(f"the multiplication value is : {result}")
-else:
-    result =num1 / num2
-    print(f"the division value is : {result}")
+#operator = input("enter a operator (+,-,*,/)")
+#num1 = float(input("enter first number: "))
+#num2 = float(input("enter second number:"))
+#print(f"you entered: {num1} {operator} {num2}")
+#if operator == "+":
+#    result = num1 + num2
+#    print(f"the addtion value is : {result}")
+#elif operator == "-":
+#    result = num1 - num2
+#    print(f"the subtraction value is : {result}")
+#elif operator == "*":
+#    result = num1 *num2
+#    print(f"the multiplication value is : {result}")
+#else:
+#   result =num1 / num2
+#    print(f"the division value is : {result}")
     
+    
+    
+#python weight converter
 
+#weight = float(input("enter your weight"))
+#unit = input("enter the weight in (K or L): ")    # in kilograms or pounds
+#if unit == "K":
+#    weight = weight * 2.20462
+#    unit = "lbs"
+#elif unit == "L":
+#    weight = weight / 2.20462
+#    unit = "kg"
+#else:
+#    print("unit is invalid")
+    
+#print(f"your weight is : {round(weight, 3)} {unit}")
+
+
+#python temperature converter
+
+#temp = float(input("enter the temperature:"))
+#unit = input("enter the unit (K or F): ")   # in kelvin or fahrenheit
+#if unit == "K":
+#    temp = temp-273.15
+#    unit = "F"
+#elif unit == "F":
+#    temp = (temp - 32) * 5/9
+#    unit = "K"
+#else:
+#    print("unit is invalid")
+    
+#print(f"the temperature is : {round(temp,2)} {unit}")
  
     
