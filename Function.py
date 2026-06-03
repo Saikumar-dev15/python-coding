6a15c10c806bbcd9280b56f21a1d50695d9c2006
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-03 21:28:12 +0530

    Return objects

--- Function.py
+++ Function.py
@@ -21,23 +21,44 @@
 # Return = statement used to end a function
 #          and send a result back to the caller
 
-def add(x,y):
-    z=x+y
-    return z
+#def add(x,y):
+#    z=x+y
+#    return z
 
-def subtract(x,y):
-    z=x-y
-    return z
+#def subtract(x,y):
+#    z=x-y
+#    return z
 
-def multiply(x,y):
-    z=x*y
-    return z
+#def multiply(x,y):
+#    z=x*y
+#    return z
 
-def divide(x,y):
-    z=x/y
-    return z
+#def divide(x,y):
+#    z=x/y
+#    return z
 
-print(add(1,2))
-print(subtract(4,2))
-print(multiply(1,3))
-print(divide(4,2))
+#print(add(1,2))
+#print(subtract(4,2))
+#print(multiply(1,3))
+#print(divide(4,2))
+
+
+#def create_name(first, last):
+#    first = first.capitalize()
+#    last = last.capitalize()
+#    return first + " " + last
+
+#full_name = create_name("spongebob","squarepants")
+
+#print(full_name)
+
+
+
+#Default arguments = A default value for certain parameters
+#                    default is used when that arguments is omitted make your functions more flexible, reduces # of arguments
+#                    1.position 2.Default 3. keyword 4. arbitary
+
+
+def net_price(list_price, discount, tax):
+    return list_price * (1- discount)* (1 + tax)
+print(net_price(500, 0, 0.05))
