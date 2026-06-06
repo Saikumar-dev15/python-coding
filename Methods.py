a6c2130e6801cdc243065a8e216ff264acc37469
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-06 22:55:41 +0530

    Intial commit

---
+++ Methods.py
@@ -0,0 +1,6 @@
+#Static methods = Amethod that belong to a class rather than any object from that class (instance)
+#                 Usually  used for general utility functions
+
+#Instance methods = Best for operation on inheritce of the class(object)
+#static methods = best for utility function that do not need accesss to class data
+
