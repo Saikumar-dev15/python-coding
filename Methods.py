22dea0f7b7c9b45b0ff1bca32027ff811a415c47
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-07 13:03:29 +0530

    Static method

--- Methods.py
+++ Methods.py
@@ -4,3 +4,25 @@
 #Instance methods = Best for operation on inheritce of the class(object)
 #static methods = best for utility function that do not need accesss to class data
 
+class Employee:
+    def __init__(self, name, position):
+        self.name = name 
+        self.position = position
+        
+    def get_info(self):
+        return f"{self.name} = {self.position}"
+    
+    @staticmethod
+    def is_valid_position(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+        return position in valid_positions
+    
+employee1 = Employee("Sai kumar", "Manager")    
+employee2 = Employee("Shiva", "Cashier")
+employee3 = Employee("Jack", "Cook")
+
+
+print(Employee.is_valid_position("Manager"))
+print(employee1.get_info())
+print(employee2.get_info())
+print(employee3.get_info())
