c12cbc60bbc539004084ae1aa58b088fb14c169e
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-07 22:49:57 +0530

    Decorators

--- Decorator.py
+++ Decorator.py
@@ -58,6 +58,34 @@ rectangle = Rectangle(3,4)
 
 
 
+#Decorator = A Function that extends the behavior of another function
+#            w/o modifying the base function 
+#            Pass the base function as an argument to the decorator
+
+#            @add_sprinkles
+#            get_ice_cream("vanilla")
+
+def add_sprinkles(func):
+    def wrapper(*args, **kwargs):
+        print("*you add sprinkles 🎊")
+        func(*args, **kwargs)
+    return wrapper
+
+def add_fudge(func):
+    def wrapper(*args, **kwargs):
+        print("*You add fudge 🍫")
+        func(*args, **kwargs)
+    return wrapper
+
+@add_sprinkles
+@add_fudge
+
+def get_ice_cream(flavour):
+    print("Here is your {flavour} ice cream🧁")
+    
+get_ice_cream("Vanilla")
+
+
 
 
 
