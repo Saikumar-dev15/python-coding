23f380497620e5cd2409765aa92d5c83b6c2a8b4
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-08 20:24:45 +0530

    Basic File Handling code

---
+++ File Handling.py
@@ -0,0 +1,17 @@
+#python file detection
+
+import os
+
+file_path = "C:/Users/LAXMI SAI KUMAR/OneDrive/Documents/Desktop/Train.txt"
+ 
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists")
+    
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif os.path.isdir(file_path):
+        print("That is a directory")
+        
+else:
+    print("That Location doesn't exist")
+    
