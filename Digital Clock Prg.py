ad345a7da01d69194956525a904617084fe74ccb
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-15 14:43:24 +0530

    Completed Digital clock time

--- Digital Clock Prg.py
+++ Digital Clock Prg.py
@@ -1,12 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication , QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import QTimer , QTime, Qt
+from PyQt5.QtGui import QFont,  QFontDatabase
 
 
 class DigitalClock(QWidget):
     def __init__(self):
         super().__init__()
-        self.time_label = QLabel("12:00:00",self)
+        self.time_label = QLabel(self)
         self.timer = QTimer(self)
         self.initUI()
         
@@ -14,9 +15,30 @@ class DigitalClock(QWidget):
         self.setWindowTitle("Digital Clock")
         self.setGeometry(600, 400, 300, 100)
         
-        vbox = QVBoxLayout()
+        vbox = QVBoxLayout()                                     #vertically time displayed
         vbox.addWidget(self.time_label)
         self.setLayout(vbox)
+        
+        self.time_label.setAlignment(Qt.AlignCenter)
+        
+        self.time_label.setStyleSheet("font-size : 150px;"
+                                      "color : #F3E88F;")
+        
+        self.setStyleSheet("background-color: black;")
+        
+        font_id = QFontDatabase.addApplicationFont("DS-DIGIT.TTF")                  #cutomised fonts....
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        my_font = QFont(font_family, 150)
+        self.time_label.setFont(my_font)
+        
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)                                             #for every 1000miliseconds it will  trigger
+        
+        self.update_time()
+        
+    def update_time(self):
+        current_time = QTime.currentTime().toString("hh:mm:ss  AP")
+        self.time_label.setText(current_time)
         
     
 if __name__ == '__main__':
