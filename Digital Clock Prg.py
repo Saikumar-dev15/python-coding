731323e9e3edbf69933871450070285a7fd83fa1
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-15 13:40:56 +0530

    Basic UI created

---
+++ Digital Clock Prg.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5.QtWidgets import QApplication , QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer , QTime, Qt
+
+
+class DigitalClock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time_label = QLabel("12:00:00",self)
+        self.timer = QTimer(self)
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(600, 400, 300, 100)
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+        
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    clock = DigitalClock()
+    clock.show()
+    sys.exit(app.exec_())
