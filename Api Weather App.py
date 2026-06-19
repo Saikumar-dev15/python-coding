67776a8ca471a58fc8b317aeee1c37a95d95289d
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-19 23:08:02 +0530

    Completed interface in Vertical columns

--- Api Weather App.py
+++ Api Weather App.py
@@ -6,8 +6,29 @@ from PyQt5.QtCore import Qt
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.city_label = QLabel("Enter city Name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("70 F",self)
+        self.emoji_label = QLabel("☀️")
+        self.description_label = QLabel("Sunny", self)
+        self.initUI()
         
-
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+        
+        vbox = QVBoxLayout()
+        
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+        
+        self.setLayout(vbox)
+        
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     weather_app = WeatherApp()
