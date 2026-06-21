7428c91ccce9890b027238839bec69c166aed049
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-06-21 23:59:13 +0530

    Completed Weather UI

--- Api Weather App.py
+++ Api Weather App.py
@@ -65,29 +65,95 @@ class WeatherApp(QWidget):
         self.get_weather_button.clicked.connect(self.get_weather)
         
     def get_weather(self):
-         
         api_key = "e85720559ffbeae9799b8cb0a325cd64"
         city = self.city_input.text()
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}" 
         
-        response = requests.get(url)
-        data = response.json()
         
-        if data["cod"] == 200:
+        try:
+          response = requests.get(url)
+          response.raise_for_status()
+          data = response.json()
+        
+          if data["cod"] == 200:
             self.display_weather(data)
         
-            
+        except requests.exceptions.HTTPError as http_error:
+            match response.status_code:
+                case 400:
+                    self.display_error("Bad request:\nPlease check your input")
+                case 401:
+                    self.display_error("Unauthorized:\nInvalid API key")                         # when your API key is inactive
+                case 403:
+                    self.display_error("Forbidden:\nAccess is denied")
+                case 404:
+                    self.display_error("Not found:\nCity not found")
+                case 500:
+                    self.display_error("Internal Server Error:\nPlease try again later")
+                case 502:
+                    self.display_error("Bad Gateway:\nInvalid response from the server")
+                case 503:
+                    self.display_error("Service Unavailable:\nServer is down")
+                case 504:
+                    self.display_error("Gateway Timeout:\nNo response from the server")
+                case _:
+                    self.display_error(f"HTTP error occured:\n{http_error}")
+                    
+    
+        
+        
+        except requests.exceptions.RequestException:
+            self.display_error("Connection Error: \nCheck your internet connection")
+        
+        except requests.exceptions.Timeout:
+            self.display_error("Timeout  Error: \nThe request timed out")
+        
+        except requests.exceptions.ToomanyRedirects:
+            self.display_error("Connection Error: \nCheck the URL")
+        
+        except requests.exceptions.RequestException as req_error:
+            self.display_error("Request Error: \n{req_error}")
         
         
     def display_error(self, message):
-        pass
+        self.temperature_label.setStyleSheet("font-size: 30px;")
+        self.temperature_label.setText(message)
+        self.emoji_label.clear()
+        self.description_label.clear()
     
     def display_weather(self, data):
-        print(data)
-    
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    weather_app = WeatherApp()
-    weather_app.show()
-    sys.exit(app.exec_()) 
+        self.temperature_label.setStyleSheet("font-size: 30px;")
+        temperature_k = data["main"]["temp"]
+        temperature_c = temperature_k - 273.15
+        temperature_f = (temperature_k * 9/5 ) -459.67
+        weather_id = data["weather"][0]["id"]
+        weather_description = data["Weather"][0]["description"]
+        
+        self.temperature_label.setText(f"{temperature_f: .0f}°F")
+        self.emoji_label.setText(self.get_weather_emoji(weather_id))
+        self.description_label.setText(weather_description)
+
+    @staticmethod
+    def get_weather_emoji(weather_id):
+        if 200 <= weather_id <= 232:
+            return "⛈️"
+        elif 300 <= weather_id <= 321:
+            return "🌦️"
+        elif 500 <= weather_id <= 531:
+            return "🌧️"
+        elif 600 <= weather_id <= 622:
+            return "❄️"
+        elif 701 <= weather_id <= 781:
+            return "🌫️"
+        elif weather_id == 800:
+            return "☀️"
+        elif weather_id == 801:
+            return "🌤️"
+        elif weather_id == 802:
+            return "⛅"
+        elif weather_id in (803, 804):
+            return "☁️"
+        else:
+            return "🌈"
+
         
